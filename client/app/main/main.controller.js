e0f66a6c63f5b6e85b3a9b12945e315373df39a6
Blackcat <l3lackcat@hotmail.com>
2016-03-12 11:32:07 +0700

    Added search criteria for room

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,20 +1,65 @@
 'use strict';
 
 angular.module('officeManagementApp')
-  .controller('MainController', function ($scope, $http) {
-    function getBuildingList () {
-      $http.get('/api/buildings').success(function(buildingList) {
-        $scope.buildingList = buildingList;
-      });
-    }
+  .controller('MainController', function ($q, $scope, $http) {
+    var updateTableThrottle = _.throttle(updateTable, 500);
+
+    // function getBuildingList () {
+    //   $http.get('/api/buildings').success(function(buildingList) {
+    //     $scope.buildingList = buildingList;
+    //   });
+    // };
 
     function getRoomList () {
       $http.get('/api/rooms').success(function(roomList) {
-        $scope.roomList = roomList;
+        $scope.roomList = angular.copy(roomList);
+        $scope.updateTableThrottle();
       });
     }
     
-    $scope.buildingList = getBuildingList();
+    function updateTable () {
+      $scope.filteredRoomList = [];
+
+      $q.all([
+        applyFilter()
+      ])
+      .then(function (results) {
+        // Do Nothing
+      });
+    };
+
+    function checkBetween (value, lower, upper) {
+      if ((lower === '') && (upper === '')) { return true; }
+      var isBetween = false;
+
+      if (lower === '') { lower = 0; }
+      if (upper === '') { upper = Infinity; }
+
+      if ((value >= lower) && (value <= upper)) {
+        isBetween = true;
+      }
+
+      return isBetween;
+    };
+
+    function applyFilter () {
+      for(var i = 0; i < $scope.roomList.length; i++) {
+        var room = $scope.roomList[i];
+
+        if ((checkBetween(room.space, $scope.spaceFilter.lower, $scope.spaceFilter.upper) === true) &&
+            (checkBetween(room.price, $scope.priceFilter.lower, $scope.priceFilter.upper) === true) &&
+            (checkBetween(room.space * room.price, $scope.netPriceFilter.lower, $scope.netPriceFilter.upper) === true)) {
+          $scope.filteredRoomList.push(room);
+        }
+      }
+    };
+
+    // $scope.buildingList = getBuildingList();
     $scope.roomList = getRoomList();
+    $scope.filteredRoomList = [];
+    $scope.netPriceFilter = { lower: '', upper: '' };
+    $scope.priceFilter = { lower: '', upper: '' };
+    $scope.spaceFilter = { lower: '', upper: '' };
+    $scope.updateTableThrottle = updateTableThrottle;
   });
 
